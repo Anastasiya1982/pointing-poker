fb97cb20359fb650d91c457d3cb24827652bd8a8
PaulRomanov <ramosha@mail.ru>
2021-09-04 00:22:18 +0300

    feat: add style

--- pointer-poker/src/component/footer/Footer.tsx
+++ pointer-poker/src/component/footer/Footer.tsx
@@ -7,25 +7,27 @@ const Footer = () => {
       <div className="line-footer">
         <div className="members-wrapper">
           <div className="member1">
-            <a className="member-link" href="https://github.com/Anastasiya1982">
+            <a className="member-link" href="https://github.com/Anastasiya1982" target="_blank">
               Sidarovich A.
             </a>
           </div>
           <div className="member2">
-            <a className="member-link" href="https://github.com/ValeryiaMIRON">
+            <a className="member-link" href="https://github.com/ValeryiaMIRON" target="_blank">
               Miron V.
             </a>
           </div>
           <div className="member3">
-            <a className="member-link" href="https://github.com/PaulRomanov/">
+            <a className="member-link" href="https://github.com/PaulRomanov/" target="_blank">
               Romanov P.
             </a>
           </div>
         </div>
         <div className="year">2021</div>
         <div className="rsschool-wrapper">
-            <a className="rsschool-link" href="https://rs.school/react/">
-            <img src="../../assets/rs_school_js.svg" alt="rs_logo" />
+            <a className="rsschool-link" href="https://rs.school/react/" target="_blank">
+              <figure> 
+                <img className="rsschool-img" src="../../assets/rs_school_js.svg" alt="rs_logo" /> 
+              </figure>
             </a>
           </div>
       </div>
