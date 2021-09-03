79a6a0d4024c1d4d5130bb15e70818bcda093594
ACER <mahanasty@mail.ru>
2021-09-03 18:43:53 +0300

    fix eslint settings

--- pointer-poker/src/App.tsx
+++ pointer-poker/src/App.tsx
@@ -3,6 +3,7 @@ import style from './App.module.css';
 import Main from "./pages/Main/Main";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Chat from "./pages/ChatComponent/Chat";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Header/>
         <>
         <Main/>
+        <Chat/>
         {/*<Game/>*/}
         {/*<LobbyPage/>*/}
         {/*<Results/>*/}
