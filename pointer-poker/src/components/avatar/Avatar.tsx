214322c47b2a139a2017736910e43a4b025c42b2
ValeryiaMIRON <lapenko69valeria@mail.ru>
2021-09-04 13:42:09 +0300

    feat: add component Avatar

---
+++ pointer-poker/src/components/avatar/Avatar.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './avatar.css';
+
+const Avatar = () => {
+  return (
+    <div className="avatar-container">
+      <div className="avatar-info" id="avatar">
+        <div className="avatar-info-name" id="name">
+          PP
+        </div>
+      </div>
+      <div className="avatar-image">
+        <img src="" alt="avatar" />
+      </div>
+    </div>
+  );
+};
+
+export default Avatar;
