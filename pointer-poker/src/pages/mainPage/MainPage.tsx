796f41c98c941fbe00b8fd53bfa914d285d12136
PaulRomanov <ramosha@mail.ru>
2021-09-04 08:47:14 +0300

    feat: add Main page

--- pointer-poker/src/pages/mainPage/MainPage.tsx
+++ pointer-poker/src/pages/mainPage/MainPage.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import ContentMainPage from '../../component/contentMainPage/ContentMainPage';
 import Footer from '../../component/footer/Footer';
 import Header from '../../component/header/Header';
+import './mainPage.css';
 
 const MainPage = () => {
-  return <div>
+  return <div className="wrapper-main_page">
     <Header />
+    <ContentMainPage />
     <Footer />
   </div>
 };
