313d599645955f50433d2f3cb48e936ddead9f34
ValeryiaMIRON <lapenko69valeria@mail.ru>
2021-09-04 13:44:20 +0300

    feat: connect components with App

--- pointer-poker/src/App.tsx
+++ pointer-poker/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import Main from './pages/Main/Main';
+import Avatar from './components/avatar/Avatar';
+import Plate from './components/plate/Plate';
 
 import './styles.scss';
 
 export const App = () => {
   return (
     <div>
-      <Main />
+      <Plate />
+      <Avatar />
     </div>
   );
 };
