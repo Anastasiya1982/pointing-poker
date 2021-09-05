418a6f268ea5762b99c4544944f2a365d6fbeaf3
ValeryiaMIRON <lapenko69valeria@mail.ru>
2021-09-05 18:28:37 +0300

    refactor: correct mistakes

--- pointer-poker/src/components/avatar/Avatar.tsx
+++ pointer-poker/src/components/avatar/Avatar.tsx
@@ -1,10 +1,10 @@
 import React, { FC } from 'react';
 import './avatar.css';
 
-type Props = {
+interface Props {
   img?: string;
   fallbackText: string;
-};
+}
 
 const Avatar: FC<Props> = ({ img, fallbackText }) => {
   return <div className="avatar-info">{img ? <img src={img} alt="" /> : fallbackText}</div>;
--- pointer-poker/src/components/plate/Plate.tsx
+++ pointer-poker/src/components/plate/Plate.tsx
@@ -1,25 +1,12 @@
 import React, { FC } from 'react';
 import './plate.css';
-import classNames from 'classnames';
-import Avatar from '../avatar/Avatar';
 
-type PropsPlate = {
-  name: string;
-  job: string;
-  // className: string;
-};
+interface PropsPlate {
+  children: string;
+}
 
-const Plate: FC<PropsPlate> = ({ name, job }) => {
-  // const g = classNames('plate', className);
-  const classDate = classNames('name');
-  const classJob = classNames('job');
-  return (
-    <div className="plate">
-      <div className={classDate}>{name}</div>
-      <div className={classJob}> {job}</div>
-      <Avatar fallbackText="MN" />
-    </div>
-  );
+const Plate: FC<PropsPlate> = ({ children }) => {
+  return <div className="plate">{children}</div>;
 };
 
 export default Plate;
