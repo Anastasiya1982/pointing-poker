7fb0c8beb8ca8dd001a1dd21ab6358ca99239569
ACER <mahanasty@mail.ru>
2021-09-06 09:43:20 +0300

    change the name of folder

--- pointer-poker/src/App.tsx
+++ pointer-poker/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Main from './pages/Main/Main';
+import ComponentForCheckingBackend from './pages/CheckngBackEnd/ComponentForCheckingBackend';
 
 import './styles.scss';
 
 export const App = () => {
   return (
     <div>
-      <Main />
+      <ComponentForCheckingBackend />
     </div>
   );
 };
