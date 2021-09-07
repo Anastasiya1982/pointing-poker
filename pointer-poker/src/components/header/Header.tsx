204f8a1a2c461ec1f1537af7eb45717318809f11
PaulRomanov <ramosha@mail.ru>
2021-09-07 13:11:46 +0300

    feat: add Header component

---
+++ pointer-poker/src/components/header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './header.scss';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="line-1" />
+      <div className="line-2" />
+      <img className="heder-logo" src="../../assets/logo.svg" alt="logo" />
+    </div>
+  );
+};
+
+export default Header;
