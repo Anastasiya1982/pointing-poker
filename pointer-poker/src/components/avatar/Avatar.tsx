c3673f06d5d389b53c96a7461b1e7e7826dc8d65
PaulRomanov <ramosha@mail.ru>
2021-09-12 14:20:47 +0300

    feat: add props ID

--- pointer-poker/src/components/avatar/Avatar.tsx
+++ pointer-poker/src/components/avatar/Avatar.tsx
@@ -3,14 +3,14 @@ import React, { FC } from 'react';
 import './avatar.scss';
 
 interface Props {
-  img?: string  | undefined | HTMLImageElement;
-  fallbackText?: string;
+  img?: string;
+  fallbackText: string;
   className?: string;
-  id?: string;
 }
 
-const Avatar: FC<Props> = ({ img, fallbackText, id }) => {
-  return <div className="avatar-info">{img ? <img src={img} id={id} alt="" /> : fallbackText}</div>;
+const Avatar: FC<Props> = ({ img, fallbackText, className }) => {
+  const classes = classNames('avatar-info', className);
+  return <div className={classes}>{img ? <img src={img} alt="" /> : fallbackText}</div>;
 };
 
 export default Avatar;
