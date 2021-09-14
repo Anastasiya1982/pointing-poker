0c139624cd83bbb1dc91140ffd1606f0581cba4a
PaulRomanov <ramosha@mail.ru>
2021-09-14 13:47:21 +0300

    resolve conflict

--- pointer-poker/src/pages/contentMainPage/ContentMainPage.tsx
+++ pointer-poker/src/pages/contentMainPage/ContentMainPage.tsx
@@ -1,50 +1,94 @@
-import React, { useCallback, useState } from 'react';
+// eslint-disable-next-line no-use-before-define
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import Button from '../../components/button/Button';
 import Input from '../../components/input/Input';
 import ModalView from '../modalView/ModalView';
 import './contentMainPage.scss';
-import BtnSwitch from '../../components/btnSwitch/BtnSwitch';
-import Avatar from '../../components/avatar/Avatar';
-import RegistrationForm from '../../components/RegistrationForm/RegistrationForm';
+
+import { io } from 'socket.io-client';
+
+
+const CONNECTION_PORT = 'http://localhost:5000';
+let  socket:any;
 
 const ContentMainPage = () => {
   const [modalActive, setModalActive] = useState(false);
+  const [roomId, setRoomId] = useState<number | null>(5);
+  const [userList, setUserList] = useState([]);
+  const [userName, setUserName] = useState('');
+  const [userInit,setUserInit]=useState(false);
 
-  const openModalStartGame = useCallback(() => {
+const geterateRandomId=()=>{
+  const id=Math.floor(Math.random() *100);
+  return id;
+}
+
+  useEffect(() => {
+    socket = io(CONNECTION_PORT);
+  }, [CONNECTION_PORT]);
+
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-shadow
+    socket.on('ROOM:show all users in Room', (users: any) => {
+     setUserList( users);
+    });
+  }, [userList]);
+
+  const createRoom = () => {
+    setRoomId(5);
+    socket.emit('ROOM:JOIN', roomId);
     setModalActive(true);
-  }, []);
+  };
+
+  function sendUser() {
+    const user = {
+      firstName: userName,
+      lastName: 'www',
+      id:geterateRandomId()
+    };
+    // @ts-ignore
+    setUserList([...userList, { ...user }]);
+    socket.emit('USER:JOIN ROOM', { roomId, user });
+    setUserInit(true);
+    setUserName('');
+  };
+
+ const  deleteUser=(id:number)=> {
+   console.log(id);
+   socket.emit("User:delete", id);
+   //  удаляем юзера из стора
+
+ }
+
+
+  console.log(userList);
+
 
   return (
     <div className="wrapper-content">
-      <img className="pp-img" src="../../assets/Group.svg" alt="pp_logo" />
+      <img className="pp-img" src="../../assets/Group.svg" alt="pp_logo"/>
       <div className="wrapper-start_game">
         <h2 className="h2-main_page">Start your planning:</h2>
         <span>Create session:</span>
-        <Button label="Start new game" TypeBtn="filled" onClick={openModalStartGame} />
+        <Button label="Start new game" TypeBtn="filled" onClick={createRoom}/>
       </div>
       <div className="wrapper-connect">
         <h2 className="h2-main_page">OR:</h2>
         <p>Connect to lobby by URL:</p>
-        <Input
-        // value={value}
-        // onChange={onChange}
-        />
+        {/*<Input*/}
+        {/*  // value={value}*/}
+        {/*  // onChange={onChange}*/}
+        {/*/>*/}
 
-        <Button label="Connect" TypeBtn="filled" onClick={() => console.log('click')} />
+        <Button label="Connect" TypeBtn="filled" onClick={() => console.log('click')}/>
 
         <ModalView active={modalActive} setActive={setModalActive}>
-          <div className="wrapper-modal">
-            <div className="wrapper-header">
-              <div className="name-modal">Connect to lobby</div>
-              <div className="name-button">Connect as Observer</div>
-              <div className="wrapper-button">
-                <BtnSwitch callback={function (): void {}} />
-              </div>
-            </div>
-            <div className="wrapper-form">
-              <RegistrationForm setModalIsActive={setModalActive} />
-            </div>
-          </div>
+            <p>
+              successfully connect: roomId:{roomId}
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure nemo maxime ad sequi
+              sapiente harum eum odit ducimus, necessitatibus placeat! Doloremque eaque suscipit,
+              fugiat nihil error tenetur corporis rem dolorem!
+            </p>
         </ModalView>
       </div>
     </div>
