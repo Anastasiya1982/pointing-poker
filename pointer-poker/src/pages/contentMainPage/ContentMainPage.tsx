0bc2a2d92d600652f669d6f695f4811cf9ea5f7d
PaulRomanov <ramosha@mail.ru>
2021-09-14 13:40:10 +0300

    fix: mentor's remark

--- pointer-poker/src/pages/contentMainPage/ContentMainPage.tsx
+++ pointer-poker/src/pages/contentMainPage/ContentMainPage.tsx
@@ -1,36 +1,18 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useState } from 'react';
 import Button from '../../components/button/Button';
 import Input from '../../components/input/Input';
 import ModalView from '../modalView/ModalView';
 import './contentMainPage.scss';
-import BtnSwitch from './../../components/btnSwitch/BtnSwitch';
+import BtnSwitch from '../../components/btnSwitch/BtnSwitch';
 import Avatar from '../../components/avatar/Avatar';
-
+import RegistrationForm from '../../components/RegistrationForm/RegistrationForm';
 
 const ContentMainPage = () => {
-
   const [modalActive, setModalActive] = useState(false);
 
-  const openModalStartGame = useCallback(
-    () => {
-      setModalActive(true);
-    },
-    [],
-  );
-
-  // загрузка аватара
-  const fileInput = document.querySelector('input[type="file"]');
-  const imageContainer = document.querySelector('.image-container');
-  let myImg = document.getElementById('img') as HTMLImageElement
-
-  fileInput?.addEventListener('change', function (e) {
-    const file = fileInput.files[0];
-    const reader = new FileReader();
-    reader.onload = () => {
-      myImg.src = reader.result;
-    }
-    reader.readAsDataURL(file);
-  });
+  const openModalStartGame = useCallback(() => {
+    setModalActive(true);
+  }, []);
 
   return (
     <div className="wrapper-content">
@@ -38,11 +20,7 @@ const ContentMainPage = () => {
       <div className="wrapper-start_game">
         <h2 className="h2-main_page">Start your planning:</h2>
         <span>Create session:</span>
-        <Button
-          label={'Start new game'}
-          TypeBtn={'filled'}
-          onClick={openModalStartGame}
-        />
+        <Button label="Start new game" TypeBtn="filled" onClick={openModalStartGame} />
       </div>
       <div className="wrapper-connect">
         <h2 className="h2-main_page">OR:</h2>
@@ -52,11 +30,7 @@ const ContentMainPage = () => {
         // onChange={onChange}
         />
 
-        <Button
-          label={'Connect'}
-          TypeBtn={'filled'}
-          onClick={() => console.log('click')}
-        />
+        <Button label="Connect" TypeBtn="filled" onClick={() => console.log('click')} />
 
         <ModalView active={modalActive} setActive={setModalActive}>
           <div className="wrapper-modal">
@@ -64,67 +38,17 @@ const ContentMainPage = () => {
               <div className="name-modal">Connect to lobby</div>
               <div className="name-button">Connect as Observer</div>
               <div className="wrapper-button">
-                <BtnSwitch callback={function (): void { }} />
+                <BtnSwitch callback={function (): void {}} />
               </div>
             </div>
             <div className="wrapper-form">
-              <form className="content-form">
-                <label>Your first name:</label>
-                <Input
-                  className={'input-modal'}
-                />
-                <label>Your last name:</label>
-                <Input
-                  className={'input-modal'}
-                />
-                <label>Your job position:</label>
-                <Input
-                  className={'input-modal'}
-                />
-              </form>
-            </div>
-            <div className="wrapper-img">
-              <label>Image:</label>
-              <div>
-                <input className="custom-file-input" id="btnInput" name="upload" type="file" title=" " />
-                <Button
-                  label={'Button'}
-                  TypeBtn={'filled'}
-                  onClick={() => console.log('Button')}
-                  className={'btn-modal-img'}
-                />
-              </div>
-
-
-              <div className="image-container" id="img-reset">
-                {/* <canvas id="canvas"></canvas> */}
-                <Avatar id={"img"} img={"myImg"} />
-                {/* <img  id="img" className="startImg" src="assets/img/img.jpg" alt="image" /> */}
-              </div>
-
-
-
-            </div>
-            <div className="wrapper-footer">
-              <Button
-                label={'Confirm'}
-                TypeBtn={'filled'}
-                onClick={() => console.log('Confirm')}
-              />
-              <Button
-                label={'Cancel'}
-                TypeBtn={'unfilled'}
-                onClick={() => setModalActive(false)}
-              />
+              <RegistrationForm setModalIsActive={setModalActive} />
             </div>
           </div>
         </ModalView>
-
       </div>
-
-
     </div>
-  )
-}
+  );
+};
 
 export default ContentMainPage;
