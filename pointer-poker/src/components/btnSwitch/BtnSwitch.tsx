e59dbe950e4b1a826012b2ded9b259250d663ac3
ValeryiaMIRON <lapenko69valeria@mail.ru>
2021-09-16 21:19:14 +0300

    feat: add props

--- pointer-poker/src/components/btnSwitch/BtnSwitch.tsx
+++ pointer-poker/src/components/btnSwitch/BtnSwitch.tsx
@@ -1,17 +1,17 @@
-import React, { FC, useState } from 'react';
+import React, { ChangeEvent, FC, useState } from 'react';
 import './btnSwitch.scss';
 
 interface Props {
-  callback: () => void,
+  callback?: () => void;
+  checked: boolean;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  id: string;
 }
 
-const BtnSwitch: FC<Props> = (props: Props) => {
-
-  const [toggle, setToggle] = useState(false);
-
+const BtnSwitch: FC<Props> = ({ checked, onChange, id }) => {
   return (
-    <label htmlFor="sw" className="switch" onClick={props.callback}>
-      <input type="checkbox" id="sw" />
+    <label htmlFor={id} className="switch">
+      <input type="checkbox" id={id} checked={checked} onChange={onChange} />
       <span className="slider round" />
     </label>
   );
