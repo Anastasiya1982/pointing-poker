d46277f91859cc48c193f39df2169a1bf56fd408
ValeryiaMIRON <lapenko69valeria@mail.ru>
2021-09-16 21:19:47 +0300

    feat: add settings

--- pointer-poker/src/pages/contentLobbyPage/ContentLobbyPage.tsx
+++ pointer-poker/src/pages/contentLobbyPage/ContentLobbyPage.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { ChangeEvent, FC, useCallback, useEffect, useState } from 'react';
 import Avatar from '../../components/avatar/Avatar';
 import Button from '../../components/button/Button';
 import Input from '../../components/input/Input';
@@ -8,21 +8,59 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPen, faPlus, faDivide } from '@fortawesome/free-solid-svg-icons';
 import ModalView from '../modalView/ModalView';
 import SelectModal from '../../components/selectModal/SelectModal';
+import BtnSwitch from '../../components/btnSwitch/BtnSwitch';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import {
+  setIsScrumMasterAPlayer,
+  setIsTimerNeeded,
+  setScoreType,
+} from '../../redux/game/gameReducer';
+import Countdown from '../../components/timer/Timer';
 
-const ContentLobbyPage = () => {
+interface Props {
+  date: string;
+}
+
+const ContentLobbyPage: FC<Props> = ({ date }) => {
+  const dispatch = useAppDispatch();
 
   const [modalActive, setModalActive] = useState(false);
 
-  const openModalAddIssues = useCallback(
-    () => {
-      setModalActive(true);
-    },
-    [],
-  );
+  const openModalAddIssues = useCallback(() => {
+    setModalActive(true);
+  }, []);
+
+  const [isMasterAPlayer, setIsMasterAPlayer] = useState(false);
+  const [isTimer, setIsTimer] = useState(false);
+  const score = useAppSelector((state) => state.game.scoreType);
+
+  const value = score === 'story point' ? 'SP' : '%';
+
+  const isMasterPlayer = () => {
+    setIsMasterAPlayer(!isMasterAPlayer);
+    if (!isMasterAPlayer) {
+      dispatch(setIsScrumMasterAPlayer(true));
+    } else {
+      dispatch(setIsScrumMasterAPlayer(false));
+    }
+  };
+
+  const isTimeNeeded = () => {
+    setIsTimer(!isTimer);
+    if (isTimer) {
+      dispatch(setIsTimerNeeded(true));
+    } else {
+      dispatch(setIsTimerNeeded(false));
+    }
+  };
+
+  const changeInputScoreType = (event: ChangeEvent<HTMLSelectElement>) => {
+    dispatch(setScoreType({ value: event.currentTarget.value }));
+  };
 
   return (
     <div className="lobby-page-wrapper">
-      <div className="date">Spring 23 planning (issues 13, 533, 5623, 3252, 6623, ...)</div>
+      <div className="date">{date}</div>
       <div className="lobby-page-scrum-master">
         <div className="lobby-page-scrum-master-title"> Scrum Master:</div>
         <Plate>
@@ -62,48 +100,67 @@ const ContentLobbyPage = () => {
         <div className="lobby-page-issues-plate">
           <Plate>
             <div className="lobby-page-issues-plate-title">Crete new issues</div>
-            <button
-              className="btn-add-issues"
-              onClick={openModalAddIssues}
-            >
+            <button className="btn-add-issues" onClick={openModalAddIssues}>
               <FontAwesomeIcon className="lobby-page-icon" icon={faPlus} />
             </button>
-
           </Plate>
         </div>
       </div>
       <div className="lobby-page-game-settings">
         <div className="lobby-page-game-settings-title">Game settings</div>
+        <div className="lobby-page-game-settings-container">
+          <div>Scram master as player:</div>
+          <BtnSwitch id="isMasterAPlayer" checked={isMasterAPlayer} onChange={isMasterPlayer} />
+        </div>
+        <div className="lobby-page-game-settings-container">
+          <div> Is timer needed:</div>
+          <BtnSwitch id="isTimer" checked={isTimer} onChange={isTimeNeeded} />
+        </div>
+
+        <div className="lobby-page-game-settings-container-input">
+          <div>Score type:</div>
+          <label className="lobby-page-game-settings-label" htmlFor="settings">
+            <select
+              className="lobby-page-game-settings-select"
+              name="settings"
+              onChange={changeInputScoreType}
+            >
+              <option defaultValue="story point">story point</option>
+              <option value="percent">percent</option>
+            </select>
+          </label>
+        </div>
+
+        <div className="lobby-page-game-settings-container-input">
+          <div>Score type (Short):</div>
+
+          <p className="lobby-page-game-settings-short">{value}</p>
+        </div>
+
+        <div className="lobby-page-game-settings-round-time-container">
+          <div>Round Time:</div>
+          <Countdown />
+        </div>
       </div>
       <ModalView active={modalActive} setActive={setModalActive}>
-        <div className="header-modal-lobby-addIssues">
-          Create Issue
-        </div>
+        <div className="header-modal-lobby-addIssues">Create Issue</div>
         <div className="content-modal-lobby-addIssues">
           <div className="wrapper-content-modal-lobby-addIssues">
-          <span>Title:</span>
-          <Input />
+            <span>Title:</span>
+            <Input />
           </div>
           <div className="wrapper-content-modal-lobby-addIssues">
-          <span>Link:</span>
-          <Input />
+            <span>Link:</span>
+            <Input />
           </div>
           <div className="wrapper-content-modal-lobby-addIssues">
-          <span>Priority:</span>
-          <SelectModal />
+            <span>Priority:</span>
+            <SelectModal />
           </div>
         </div>
         <div className="footer-modal-lobby-addIssues">
-          <Button
-            label={'Yes'}
-            TypeBtn={'filled'}
-            onClick={() => console.log('Confirm')}
-          />
-          <Button
-            label={'No'}
-            TypeBtn={'unfilled'}
-            onClick={() => setModalActive(false)}
-          />
+          <Button label={'Yes'} TypeBtn={'filled'} onClick={() => console.log('Confirm')} />
+          <Button label={'No'} TypeBtn={'unfilled'} onClick={() => setModalActive(false)} />
         </div>
       </ModalView>
     </div>
