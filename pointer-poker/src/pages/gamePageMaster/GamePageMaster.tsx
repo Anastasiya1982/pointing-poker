eab9c45d0b838690ab31f392a53b5fd5b3db9ee2
PaulRomanov <ramosha@mail.ru>
2021-09-20 13:57:00 +0300

    add  game page master

---
+++ pointer-poker/src/pages/gamePageMaster/GamePageMaster.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from '../../components/footer/Footer';
+import Header from '../../components/header/Header';
+import ContentGamePageMaster from '../contentGamePageMaster/ContentGamePageMaster';
+import './gamePageMaster.scss';
+
+const GamePageMaster = () => {
+  return <div className="wrapper-game-page-master">
+    <Header />
+    <ContentGamePageMaster />
+    <Footer />
+  </div>
+};
+
+export default GamePageMaster;
