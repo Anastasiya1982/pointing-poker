aab5a09c3a06caabf26fd5d08efa73fb9c52e8fb
ValeryiaMIRON <lapenko69valeria@mail.ru>
2021-09-21 18:19:34 +0300

    refactor: resolve conflicts

--- pointer-poker/src/pages/pagesAll/PagesAll.tsx
+++ pointer-poker/src/pages/pagesAll/PagesAll.tsx
@@ -1,4 +1,4 @@
-import { Route, Switch, useLocation } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Main from '../Main/Main';
 import LobbyPage from '../LobbyPage/LobbyPage';
 import React from 'react';
