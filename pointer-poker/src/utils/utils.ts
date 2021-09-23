b4e02ba88965e9ce5efbd26e639061f6b3231af5
PaulRomanov <ramosha@mail.ru>
2021-09-23 12:28:26 +0300

    add funcPercent

--- pointer-poker/src/utils/utils.ts
+++ pointer-poker/src/utils/utils.ts
@@ -20,3 +20,21 @@ export const generateRandomNumber = () => {
   const n = Math.floor(Math.random() * 30) + 1;
   return n;
 };
+
+export const funcPercent = function toPercentages(arrTest: []) {
+  var result = arrTest.reduce(function (acc, el) {  
+    acc[el] = (acc[el] || 0) + 1;
+    return acc;
+  }, {});
+  const arrValueFromObj = Object.values(result); 
+  const sumValues = result => Object.values(result).reduce((a, b) => a + b); 
+  let numberPercent = 0;
+  let arrValueFromObjNEW = [];
+  for (let i = 0; i < arrValueFromObj.length; i++) {
+    numberPercent = arrValueFromObj[i] * (100 / sumValues(result))
+    arrValueFromObjNEW.push(numberPercent) 
+  }
+  return arrValueFromObjNEW.map(function (x: number) {
+    return parseFloat((x).toFixed(2));
+  });
+}
