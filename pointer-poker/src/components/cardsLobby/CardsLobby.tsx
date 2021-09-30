0499f7d937021a0b6cb463789b4dab3e6046f352
ValeryiaMIRON <lapenko69valeria@mail.ru>
2021-09-30 20:58:43 +0300

    fix: add input for number in cards

--- pointer-poker/src/components/cardsLobby/CardsLobby.tsx
+++ pointer-poker/src/components/cardsLobby/CardsLobby.tsx
@@ -1,44 +1,36 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { ChangeEvent, useCallback, useEffect, useState } from 'react';
-import { setCards } from '../../redux/game/gameReducer';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import Card from '../card/Card';
 import './cardsLobby.scss';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
-import { generateRandomId, generateRandomNumber } from '../../utils/utils';
-
+import ModalView from '../../pages/modalView/ModalView';
+import CreateCards from '../createCards/CreateCards';
 
 const CardsLobby = () => {
+  const [modalActive, setModalActive] = useState(false);
   const dispatch = useAppDispatch();
   const cards = useAppSelector((state) => state.game.cards);
-  const selectedCard = useAppSelector((state) => state.game.selectedCard);
 
-  const handleAddCard = () => {
-    let id = generateRandomId();
-    let value = generateRandomNumber();
-   let newCards=[...cards]
-    dispatch(setCards([...newCards,{id,value}]));
-  };
+  const openModalAddCard = useCallback(() => {
+    setModalActive(true);
+  }, []);
 
   return (
     <div className="cards-lobby-container">
       <div>Add card values:</div>
       {cards.map((card, index) => {
-        return (
-          <Card
-            key={index}
-            id={card.id}
-            number={card.value}
-            className='card-container'
-          />
-        );
+        return <Card key={index} id={card.id} number={card.value} className="card-container" />;
       })}
 
       <div className="btn-add-card-container ">
-        <button onClick={handleAddCard} className="btn-add-card">
+        <button onClick={openModalAddCard} className="btn-add-card">
           <FontAwesomeIcon size="4x" icon={faPlus} />
         </button>
       </div>
+      <ModalView active={modalActive} setActive={setModalActive}>
+        <CreateCards setModalActive={setModalActive} />
+      </ModalView>
     </div>
   );
 };
---
+++ pointer-poker/src/components/createCards/CreateCards.tsx
@@ -0,0 +1,51 @@
+import React, { ChangeEvent, FC, useCallback, useState } from 'react';
+import { setCards } from '../../redux/game/gameReducer';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import { generateRandomId } from '../../utils/utils';
+import Button from '../button/Button';
+import Input from '../input/Input';
+import './createCards.scss';
+
+interface PropsCardType {
+  setModalActive: (modalActive: boolean) => void;
+}
+
+const CreateCards: FC<PropsCardType> = ({ setModalActive }) => {
+  const dispatch = useAppDispatch();
+  const cards = useAppSelector((state) => state.game.cards);
+  const [cardNumber, setCardNumber] = useState('');
+
+  const onHandleCardNumberChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setCardNumber(event.target.value);
+  };
+
+  const handleAddCard = (event: ChangeEvent<HTMLButtonElement>) => {
+    let id = generateRandomId();
+    const value = cardNumber;
+    if (value === '' || !/^[1-9]+$/.test(value)) {
+      return;
+    }
+    let newCards = [...cards];
+    dispatch(setCards([...newCards, { id, value }]));
+    setCardNumber('');
+    setModalActive(false);
+  };
+
+  return (
+    <div>
+      <div className="header-modal-lobby-addCards">Create Card</div>
+      <div className="content-modal-lobby-addCards">
+        <div className="wrapper-content-modal-lobby-addCards">
+          <span>Number:</span>
+          <Input onChange={onHandleCardNumberChange} placeholder="number" />
+        </div>
+      </div>
+      <div className="footer-modal-lobby-addCards">
+        <Button label="Yes" TypeBtn="filled" onClick={handleAddCard} />
+        <Button label="No" TypeBtn="unfilled" onClick={() => setModalActive(false)} />
+      </div>
+    </div>
+  );
+};
+
+export default CreateCards;
